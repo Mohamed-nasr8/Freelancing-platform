11bf67a41109d95c81465a458c24f141b24499f4
Gamal Mohamed <gm2081620@gmail.com>
2023-02-11 00:48:19 +0200

    First Models Creation

---
+++ Crowdsourcing.DL/Models/Proposal_Status_Catalog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crowdsourcing.DL.Models
+{
+    public class Proposal_Status_Catalog
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "varchar(128)")]
+        public string Name { get; set; }
+        public ICollection<Message> Message { get; set; }
+        public ICollection<Proposal> Proposal { get; set; }
+
+    }
+}
