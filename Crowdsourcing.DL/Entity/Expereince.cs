1998b5d4070b637d92f0e22cfd8b743008eb7cb4
Mohamed Eid <m01023598749@gmail.com>
2023-02-13 11:22:19 +0200

    Add Freelancer Entity

---
+++ Crowdsourcing.DL/Entity/Expereince.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crowdsourcing.DL.Entity
+{
+    public class Expereince
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
