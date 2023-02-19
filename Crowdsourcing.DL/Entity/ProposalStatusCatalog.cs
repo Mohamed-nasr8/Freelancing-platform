8420d8986bd609e10fbdfd8fb72f258ad09c9fb9
Gamal Mohamed <gm2081620@gmail.com>
2023-02-19 21:23:07 +0200

    entity valid

--- Crowdsourcing.DL/Entity/ProposalStatusCatalog.cs
+++ Crowdsourcing.DL/Entity/ProposalStatusCatalog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Crowdsourcing.DL.Models
     public class ProposalStatusCatalog
     {
         public int Id { get; set; }
-        [Column(TypeName = "varchar(128)")]
+        [Column(TypeName = "varchar(128)"),Required(ErrorMessage ="Requried Name")]
         public string Name { get; set; }
         public ICollection<Message> Messages { get; set; }
         public ICollection<Proposal> Proposals { get; set; }
