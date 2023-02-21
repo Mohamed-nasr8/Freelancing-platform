a4a5b4f80c231a0775f9868e0abf81a5b266c496
Mohamed Nasr <engmedonasr@gmail.com>
2023-02-21 21:54:13 +0200

    make automapper

--- Crowdsourcing.BL/Mapper/AoutomapperProfile.cs
+++ Crowdsourcing.BL/Mapper/AoutomapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Crowdsourcing.BL.Models;
 using Crowdsourcing.BL.ViewModels;
 using Crowdsourcing.DL.Entity;
 using Crowdsourcing.DL.Models;
@@ -23,6 +24,46 @@ namespace Crowdsourcing.BL.Mapper
             CreateMap<Attachment,AttachmentVM>();
             CreateMap<Proposal, ProposalVM>();
             CreateMap<ProposalStatusCatalog,ProposalStatusCatalogVM>();
+            CreateMap<UserAccount, UserAccounVM>();
+            CreateMap<UserAccounVM, UserAccount>();
+            CreateMap<Notification, NotificationVM>();
+            CreateMap<NotificationVM, Notification>();
+            CreateMap<Rating, RatingVM>();
+            CreateMap<RatingVM, Rating>();
+            CreateMap<Client, ClientVM>();
+            CreateMap<ClientVM, Client>();
+            CreateMap<Service, ServiceVM>();
+            CreateMap<ServiceVM, Service>();
+            CreateMap<OtherSkills, OtherSkillsVM>();
+            CreateMap<OtherSkillsVM, OtherSkills>();
+            CreateMap<Verification, VerificationVM>();
+            CreateMap<VerificationVM, Verification>();
+            CreateMap<FreelancerService, FreelancerServiceVM>();
+            CreateMap<FreelancerServiceVM, FreelancerService>();
+            CreateMap<Skill, SkillVM>();
+            CreateMap<SkillVM, Skill>();
+            CreateMap<PaymentType, PaymentTypeVM>();
+            CreateMap<PaymentTypeVM, PaymentType>();
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
