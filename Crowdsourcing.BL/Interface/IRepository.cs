4d5940a102ab0fa7227f993a8ae35434fdd779dd
Mohamed Nasr <engmedonasr@gmail.com>
2023-02-21 20:43:29 +0200

    creat repo

---
+++ Crowdsourcing.BL/Interface/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crowdsourcing.BL.Interface
+{
+    public interface IRepository
+    {
+        public interface IRepository<TEntity> where TEntity : class
+        {
+            TEntity Get(int id);
+            IEnumerable<TEntity> GetAll();
+            IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+            void Add(TEntity entity);
+            void Remove(TEntity entity);
+            void Update(TEntity entity);
+            int Count(Expression<Func<TEntity, bool>> predicate = null);
+        }
+
+
+
+
+    }
+}
